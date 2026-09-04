3db4f93333e00374ba6766b3ee87169f9d01c12c
Tobias Trout <tobias.w.trout@gmail.com>
2026-09-04 19:55:23 -0400

    Update PortScanner.py
    Added:
    
    - ip input validation
    - TCP/UDP scan selection 
    - UDP scanning
    - Cleaned output

--- python-port-scanner/PortScanner.py
+++ python-port-scanner/PortScanner.py
@@ -1,9 +1,62 @@
 import socket
+import ipaddress
+
+def inputcheck():
+
+    while True:
+
+        ip = input("Enter Target IP address: ")
+
+        try:
+            ipaddress.ip_address(ip)
+            protocolcheck(str(ip))
+            return False
+        
+        except ValueError:
+            print("Invalid IP address")
+
+def protocolcheck(addr):
+
+    protocol = input("TCP or UDP scan?: ")
+
+    while True:
+        if protocol.lower() == "tcp":
+            test_tcp(addr)
+            return False
+
+        elif protocol.lower() == "udp":
+            test_udp(addr)
+            return False
+
+        else:
+            print("Invalid protocol or format")
+
+
+def test_udp(host):
+    print("Checking UDP ports:")
+    port = [67, 68, 69, 123, 161, 514]
+    for i in range(6):
+        curport = port[i]
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.settimeout(2.0)
+
+        target = socket.gethostbyname(host)
+        try:
+
+            sock.sendto(b'', (target, curport))
+            sock.recvfrom(1024)
+
+        except socket.timeout:
+            print_connection(0, str(curport))
+
+        except socket.error:
+            print_connection(1, str(curport))
 
 
 def test_tcp(host):
-    port = [21, 22, 23, 25, 53, 80, 110, 135, 143, 443, 445]
-
+    port = [21, 22, 23, 25, 53, 80, 110, 143, 443, 445, 3389]
+    print("Checking TCP ports:")
     for i in range(11):
         curport = str(port[i])
         try:
@@ -31,7 +84,4 @@ def print_connection(result, num):
         return False
 
 
-target = input("enter target: ")
-
-
-test_tcp(target)
+inputcheck()
